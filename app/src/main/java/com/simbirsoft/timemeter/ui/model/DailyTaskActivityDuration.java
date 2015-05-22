c6a74acf7ab5d5235102a117c3a8b1eb6b4a07b6
d.makarov <d.makarov@simbirsoft.com>
2015-05-22 14:06:57 +0300

    Impl showing label with total task duration under graphs

--- app/src/main/java/com/simbirsoft/timemeter/ui/model/DailyTaskActivityDuration.java
+++ app/src/main/java/com/simbirsoft/timemeter/ui/model/DailyTaskActivityDuration.java
@@ -7,4 +7,12 @@ import java.util.Date;
 public class DailyTaskActivityDuration {
     public Date date;
     public TaskOverallActivity[] tasks;
+
+    public long getTotalTasksDuration() {
+        long totalDuration = 0;
+        for (int i = 0; i < tasks.length; i++) {
+            totalDuration += tasks[i].getDuration();
+        }
+        return totalDuration;
+    }
 }
