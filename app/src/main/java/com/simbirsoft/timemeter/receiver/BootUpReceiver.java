8cf0aa23dfd17b1dde11b4901697b49e39cfcdea
smile <alexander.ismailov@simbirsoft.com>
2015-06-08 17:01:56 +0300

    реализовал запуск менеджера активностей, но обнаружил что при его запуске падает главная активити

--- app/src/main/java/com/simbirsoft/timemeter/receiver/BootUpReceiver.java
+++ app/src/main/java/com/simbirsoft/timemeter/receiver/BootUpReceiver.java
@@ -4,16 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.simbirsoft.timemeter.ui.main.MainActivity_;
+import com.simbirsoft.timemeter.injection.Injection;
 
 public class BootUpReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent ativivtyIntent = new Intent(context, MainActivity_.class);
+        Intent ativivtyIntent = new Intent(context, Injection.sTaskManager.taskActivityManager().getClass());
         ativivtyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(ativivtyIntent);
-
-//        Intent serviceIntent = new Intent(context, MyService.class);
-//        context.startService(serviceIntent);
     }
 }
