eee00b6d7aed0066f2765ac622981968290baa9c
smile <alexander.ismailov@simbirsoft.com>
2015-06-09 09:02:25 +0300

    Исправил ошибку при запуске менеджера активностей при автозагрузке

--- app/src/main/java/com/simbirsoft/timemeter/receiver/BootUpReceiver.java
+++ app/src/main/java/com/simbirsoft/timemeter/receiver/BootUpReceiver.java
@@ -9,8 +9,7 @@ import com.simbirsoft.timemeter.injection.Injection;
 public class BootUpReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent ativivtyIntent = new Intent(context, Injection.sTaskManager.taskActivityManager().getClass());
-        ativivtyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(ativivtyIntent);
+        Intent serviceIntent = new Intent(context, Injection.sTaskManager.taskActivityManager().getClass());
+        context.startService(serviceIntent);
     }
 }
