19b6852c7f4652a60e647ba1df01960a440de3d8
Andrey Chernyshev <andrey.a-k-chernyshev@yandex.ru>
2015-12-03 12:04:43 +0300

    добавлен фрагмент для отображения инфы о компании

---
+++ app/src/main/java/com/simbirsoft/timemeter/ui/aboutus/AboutUsFragment.java
@@ -0,0 +1,12 @@
+package com.simbirsoft.timemeter.ui.aboutus;
+
+import com.simbirsoft.timemeter.R;
+import com.simbirsoft.timemeter.ui.main.MainFragment;
+
+import org.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.fragment_about_us)
+public class AboutUsFragment extends MainFragment{
+
+
+}
